91d5679a670a340fe460ddcb729ffdc6f9e7bda3
Hiswe <hiswehalya@gmail.com>
2018-10-14 09:05:11 +0700

    TESTS – existence test for API

---
+++ src/endpoints.test.ts
@@ -0,0 +1,33 @@
+import test from 'ava'
+import nock from 'nock'
+
+import connect from './query-etherpad'
+import checkConfiguration from './check-configuration'
+import { supportedMethods } from './_test-utils'
+
+const conf = checkConfiguration({
+  apiKey: `6b95f6d270f4f719f1b70e8ad2f742deef94c5bccee7d495250c0fbb8cecefc7`,
+  url: `http://papapad.com`,
+})
+
+const nockServer = nock(`${conf.url}/api/${conf.apiVersion}`)
+
+supportedMethods.forEach(methodName => {
+  nockServer
+    .get(`/${methodName}`)
+    .query(true)
+    .reply(200, {
+      code: 0,
+      message: 'ok',
+      data: `${methodName}`,
+    })
+})
+
+test(`call all methods`, async t => {
+  const etherpad = connect(conf)
+  // nock will throw if we call 2 times the same route
+  await Promise.all(
+    supportedMethods.map(methodName => etherpad[methodName]({}, false)),
+  )
+  t.pass(`all done`)
+})
